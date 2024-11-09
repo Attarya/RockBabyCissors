018adcdfd15a96726d3e2fbea6490d173e606c66
Attarya <andimistreanu@gmail.com>
2024-11-09 15:30:37 +0200

    w rizz

---
+++ script.js
@@ -0,0 +1,13 @@
+function pcChoice() {
+  let list = ["rock", "paper", "scissors"];
+  return list[Math.random() * list.length];
+}
+
+function myChoice() {
+  let choice = prompt("Dute fanpulamea alege").toLowerCase();
+  return choice;
+}
+
+human;
+
+function playRound(myChoice, pcChoice) {}
